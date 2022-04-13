933d65fd397a95e5a0cfeb5b82ab3b8d075b195b
John Parkhurst <jparkhurst120@gmail.com>
2022-04-13 00:15:56 -0400

    hello world

--- projects/Betting-Odds/index.js
+++ projects/Betting-Odds/index.js
@@ -1,1 +1,13 @@
 //John Parkhurst
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('John T Parkhurst!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
