e24b833113a4abeae2cbb188726ea962aeb784c8
John Parkhurst <jparkhurst120@gmail.com>
2022-04-14 00:23:00 -0400

    form
    just a simple form

--- projects/Betting-Odds/index.js
+++ projects/Betting-Odds/index.js
@@ -5,11 +5,19 @@ const app = express();
 const path = require('path');
 const port = 3000;
 
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
 // sendFile will go here
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
+app.post('/calc', function (req, res) {
+    var name = req.body.val + ' ' + req.body.favored;
 
+    res.send(name + ' Submitted Successfully!');
+});
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+    console.log(`App running on: ${port}`);
 })
