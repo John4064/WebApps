986917d4d51739f62d19e276df83034ffee6ef9e
John Parkhurst <jparkhurst120@gmail.com>
2022-04-16 22:20:29 -0400

    convert to js

--- projects/Betting-Odds/src/calc/convertor.js
+++ projects/Betting-Odds/src/calc/convertor.js
@@ -3,15 +3,15 @@
  * @param favored boolean expression if the bet is + or 0
  * @returns {number} returns the  amount of money won if the bet is win
  */
-function convertor(value, favored){
+function convertor(value,odds, favored){
     //means an underdog aka
     if(favored==='+'){
-        return 1;
+        return Math.round(odds/100.0*value);
         //Means a favorite aka -140 is 140 to win 100
     }else if (favored==='-'){
         //need to get module
-        return value+(value/100);
+        return  Math.round(100.0/odds*value);
     }else{
-        return -320;
+        return -1234;
     }
 }
